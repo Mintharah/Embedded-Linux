d3fab7f84aa6fb0581aea05dbeff13bedfe16b06
Yasmine <yasmine.elkady333@gmail.com>
2026-03-20 16:35:10 +0200

    u-boot with blinking LED on rpi

--- bootup_rpi/boot.cmd
+++
@@ -1,7 +0,0 @@
-setenv bootargs "earlycon=uart8250,mmio32,0x3f215040 console=ttyAMA1,115200 root=/dev/ram0 rw rdinit=/sbin/init"
-
-fatload mmc 0:1 ${kernel_addr_r} Image
-fatload mmc 0:1 ${fdt_addr_r} bcm2710-rpi-3-b-plus.dtb
-fatload mmc 0:1 0x03000000 uInitrd
-
-booti ${kernel_addr_r} 0x03000000 ${fdt_addr_r}
---
+++ bootup_rpi_uboot/boot/.Trash-1000/files/boot.cmd
@@ -0,0 +1,9 @@
+setenv bootargs "console=ttyS0,115200 console=tty1 keep_bootcon rdinit=/sbin/init"
+
+
+load mmc 0:1 ${kernel_addr_r} Image
+load mmc 0:1 ${fdt_addr_r} bcm2837-rpi-3-b-plus.dtb
+load mmc 0:1 ${ramdisk_addr_r} initramfs.uboot
+setenv initrd_size ${filesize}
+
+booti ${kernel_addr_r} ${ramdisk_addr_r}:${initrd_size} ${fdt_addr_r}
